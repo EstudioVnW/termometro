eab96445c84dcce912cd969a0cb36e6899ee47db
Estúdio Vai na Web 2 <anna.luiza.araujo@hotmail.com>
2018-04-09 17:13:43 -0300

    alteração de classes

--- src/components/DuvidaAluno.vue
+++ src/components/DuvidaAluno.vue
@@ -1,41 +1,56 @@
 <template>
-  <div class="pai">
-    <div class="pai-sobre-o-professor">
-      <div class="pai-sobre-inicio">
-        <img class="pai-sobre-inicio-termometro-img" src="../assets/termometro-logo.svg">
-        <img class="pai-sobre-inicio-logo-img" src="../assets/logo-VNW.svg">
+  <div class="body">
+    <div class="conteudo-descricao">
+      <div class="conteudo-descricao__img">
+        <img class="conteudo-descricao__img--logo" src="../assets/termometro-logo.svg">
+        <img class="conteudo-descricao__img--vnw" src="../assets/logo-VNW.svg">
       </div>
-      <div class="pai-sobre-expecificacoes-prof" >
-        <img class="pai-sobre-foto-professor-perfil" src="../assets/foto-perfil.png">
-          <h4 class="pai-sobre-expecificacoes-prof-nome">Rômulo Tunala</h4>
-          <h5 class="pai-sobre-expecificacoes-prof-proficao">Professor</h5>
-          <h5 class="pai-sobre-expecificacoes-prof-local" >Cpx.alemão</h5>
+      <div class="conteudo-descricao__prof" >
+        <img class="conteudo-descricao__prof--img" src="../assets/foto-perfil.png">
+          <h4 class="conteudo-descricao__prof--nome">Rômulo Tunala</h4>
+          <h5 class="conteudo-descricao__prof--usuario">Professor</h5>
+          <h5 class="conteudo-descricao__prof--local" >Cpx.alemão</h5>
       </div>  
-      <div class="pai-sobre_duvidas-retangulo-expecificaçoes">
-          <div class="pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma">
-            <div class="pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turma">
-              <p class="pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turma-M">MÓDULO 1 - </p>
-              <p class="pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turma-T">TURMA 2</p>
-            </div>
-            <p class="pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turno">Manhã</p>
-          </div>
-          <p class="pai-sobre_duvidas-retangulo-expecificaçoes-materia">Introdução ao CSS3</p>
-          <p class="pai-sobre_duvidas-retangulo-expecificaçoes-materia_sobre">
+      <div class="conteudo-descricao__detalhes">
+          <p class="conteudo-descricao__detalhes--turma">{{modulo}} - {{turma}}</p>
+          <p class="conteudo-descricao__detalhes--turno">Manhã</p>
+          <p class="conteudo-descricao__detalhes--materia">Introdução ao CSS3</p>
+          <p class="conteudo-descricao__detalhes--sobre">
             Esse módulo foi criado para você que quer iniciar no universo da programação. Nele você vai aprender a criar sites incríveis que funcionem em diferentes tamanhos de tela como computadores desktops, celulares, tablets, TVs e muito mais. É o primeiro passo para você desenvolver páginas para sites, aplicativos mobile e sistemas web e desktop.
           </p>
       </div>
     </div>
-    <div class="pai-centro_duvidas">
-      <div class="pai-centro_duvidas-lista_conteiner">
-        <h1>Centro de Dúvidas</h1>
-        <div class="pai-centro_duvidas-lista_conteiner-recados">
-          <ul class="pai-centro_duvidas-lista_conteiner-recados-ul" v-for="recado in usuario.recados">
-            <li class="pai-centro_duvidas-lista_conteiner-recados-ul-li">{{recado}}</li>
-          </ul>
-        </div> 
-        <div class="pai-centro_duvidas-caixa_comentario">
-            <input class="input-caixa-comentario" v-model="recado" v-on:keyup.enter="addRecado" placeholder="Digite a sua dúvida">
-            <button class="pai-centro_duvidas-caixa_comentario-botao_iniciar">
+    <div class="conteudo-duvidas">
+      <div class="conteudo-duvidas__container">
+        <h1 class="conteudo-duvidas__container--titulo">Centro de Dúvidas</h1>
+        <div class="conteudo-duvidas__container-bloco">
+          <div class="conteudo-duvidas__container-bloco-caixa">
+            <ul class="conteudo-duvidas__container-bloco-caixa-lista" v-for="recado in usuario.recados">
+              <li class="conteudo-duvidas__container-bloco-caixa-lista--recado">{{recado}}</li>
+            </ul>
+          </div>
+          <div class="conteudo-duvidas__container-bloco-caixa2">
+            <div class="conteudo-duvidas__container-bloco-caixa2-caixa">
+              <div class="conteudo-duvidas__container-bloco-caixa2-caixa--cor"></div>
+              <p class="conteudo-duvidas__container-bloco-caixa2-caixa--texto">Respondido</p>
+            </div>
+            <div class="conteudo-duvidas__container-bloco-caixa2-caixa">
+              <div class="conteudo-duvidas__container-bloco-caixa2-caixa--cor"></div>
+              <p class="conteudo-duvidas__container-bloco-caixa2-caixa--texto">Responder depois</p>
+            </div>
+            <div class="conteudo-duvidas__container-bloco-caixa2-caixa">
+              <div class="conteudo-duvidas__container-bloco-caixa2-caixa--cor"></div>
+              <p class="conteudo-duvidas__container-bloco-caixa2-caixa--texto">Chamar atenção</p>
+            </div>
+            <div class="conteudo-duvidas__container-bloco-caixa2-caixa">
+              <div class="conteudo-duvidas__container-bloco-caixa2-caixa--cor"></div>
+              <p class="conteudo-duvidas__container-bloco-caixa2-caixa--texto">Dar prioridade</p>
+            </div>
+          </div>
+        </div>
+        <div class="conteudo-duvidas__container-bloco2">
+            <input class="conteudo-duvidas__container-bloco2--input" v-model="recado" v-on:keyup.enter="addRecado" placeholder="Digite a sua dúvida">
+            <button class="conteudo-duvidas__container-bloco2--button">
               Enviar dúvida
             </button>
         </div>
@@ -54,8 +69,9 @@ export default {
       legenda:'',
       usuario: {
         recados: ['Não ficou claro como o sistema funciona!','Não entendi exatamente qual a ordem correta de usar os caractéres na hora de fechar uma tag.','Como faz para colocar um video rodando automaticamente quando a página carregar?','Como faz para colocar um video rodando automaticamente quando a página carregar?','Como faz para colocar um video rodando automaticamente quando a página carregar?','Como faz para colocar um video rodando automaticamente quando a página carregar?'],
-        legendas: ['Respondido','Responder depois','Chamar atenção']
-      } 
+      },
+      modulo: 'Modulo I',
+      turma: 'Turma 2'
     }
   }
 }
@@ -63,7 +79,7 @@ export default {
 
 
 <style scoped>
-.pai{
+.body{
     height: 90vh; 
     padding: 23px 6px;
     background: url(../assets/foto-fundo.jpg) no-repeat #FF8C00; 
@@ -72,7 +88,7 @@ export default {
     flex-direction: row;
     justify-content: space-around;
   }
-  .pai-sobre-o-professor{
+  .conteudo-descricao{
     width: 16vw;
     padding: 2% 0;
     background-color: #fff;
@@ -83,88 +99,75 @@ export default {
     justify-content: space-around;
     align-items: center;
   }
-  .pai-sobre-inicio{
+  .conteudo-descricao__img{
     margin-bottom: 5%;
     display: flex;
     align-items: center;
     justify-content: center;
   }
-  .pai-sobre-inicio-termometro-img{
+  .conteudo-descricao__img--logo{
     height: 12vh; 
   }
-  .pai-sobre-inicio-logo-img{
+  .conteudo-descricao__img--vnw{
     height: 7vh;
   }
-  .pai-sobre-expecificacoes-prof{
-    color: #2E3192;
+  .conteudo-descricao__prof{
     margin: 3% 0;
+    color: #2E3192;
   }
-  .pai-sobre-foto-professor-perfil{
+  .conteudo-descricao__prof--img{
     width: 7vw;
     margin: 3% 0;
-    background-color: #F26522;
     border: solid 2px #2E3192;
     border-radius: 100%;
+    background-color: #F26522;
   }
-  .pai-sobre-expecificacoes-prof-nome{
+  .conteudo-descricao__prof--nome{
     margin: 0;
     font-size: 16px;
     font-weight: bold;
   }
-  .pai-sobre-expecificacoes-prof-proficao{
+  .conteudo-descricao__prof--usuario{
     margin: 0;
     font-size: 16px;
     font-weight: normal;
   }
-  .pai-sobre-expecificacoes-prof-local{
+  .conteudo-descricao__prof--local{
     margin: 0;
     font-size: 16px;
     font-weight: normal;    
   }
-  .pai-sobre_duvidas-retangulo-expecificaçoes{
-    display: flex;
-    margin: 0 ;
-    width: 13vw;
+  .conteudo-descricao__detalhes{
+    width: 90%;
     height: 55vh;
+    margin: 0;
+    color: #2E3192;
     text-align: left;
+    display: flex;
     flex-direction: column;
     font-weight: normal;
-    color: #2E3192;
-    
   }
-  .pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turma{
-    width: 100%;
-    font-weight: normal;
-    display: flex;
-    justify-content: flex-start;
-  }
-  .pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turma-M{
+  .conteudo-descricao__detalhes--turma{
     margin: 0;
-    font-size: 14px;
-    font-weight: normal;
+    font-size: 16px;
+    font-weight: bold;
     color: #FF611E;
   }
-  .pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turma-T{
-    margin: 0;
-    font-size: 14px;
-    font-weight: normal;
-    color: #FF611E;
-  }
-  .pai-sobre_duvidas-retangulo-expecificaçoes-topo_turma-turno{
+  .conteudo-descricao__detalhes--turno{
     margin: 0;
     font-weight: bold;
   }
-  .pai-sobre_duvidas-retangulo-expecificaçoes-materia{
+  .conteudo-descricao__detalhes--materia{
     font-weight: bold;
     margin: 5% 0;
   }
-  .pai-sobre_duvidas-retangulo-expecificaçoes-materia_sobre{
+  .conteudo-descricao__detalhes--sobre{
     min-height: 25vh;
     width: 14vw;
     margin: 0;
     font-size: 14px;
   }
-  .pai-centro_duvidas{
+  .conteudo-duvidas{
     width: 80vw;
     background-color: #fff;
     border: solid 2px #2E3192;
@@ -172,9 +175,9 @@ export default {
     display: flex;
     justify-content: center;
   }
-  .pai-centro_duvidas-lista_conteiner{
+  .conteudo-duvidas__container{
     height: 90vh;
-    width: 50vw;
+    width: 60vw;
     margin: 0;
     text-align: left;
     color: #2E3192;
@@ -182,11 +185,18 @@ export default {
     flex-direction: column;
     justify-content: center;
   }
-  .pai-centro_duvidas-lista_conteiner-recados{
-    width: 35vw;
+  .conteudo-duvidas__container--titulo{
+    width: 100%;
+  }
+  .conteudo-duvidas__container-bloco{
+    display: flex;
+    justify-content: space-between;
+  }
+  .conteudo-duvidas__container-bloco-caixa{
+    width: 40vw;
     overflow: auto;
   }
-  .pai-centro_duvidas-lista_conteiner-recados-ul{
+  .conteudo-duvidas__container-bloco-caixa-lista{
     list-style-type: circle;
     overflow: auto;
     padding-left: 0; 
@@ -194,41 +204,57 @@ export default {
     border-radius: 8px;
     background-color:
   }
-  .pai-centro_duvidas-lista_conteiner-recados-ul-li{
+  .conteudo-duvidas__container-bloco-caixa-lista--recado{
     margin: 5px;
     color: #2E3192;
     list-style-type: none;
   }
-  .pai-centro_duvidas-caixa_comentario{
-    width: 50vw;
+  .conteudo-duvidas__container-bloco-caixa2{
+    width: 25%;
+    height: 30vh;
+  }
+  .conteudo-duvidas__container-bloco-caixa2-caixa{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .conteudo-duvidas__container-bloco-caixa2-caixa--cor{
+    width: 22px;
+    height: 22px;
+    border: solid 2px #2E3192;
+    border-radius: 5px;
+  }
+  .conteudo-duvidas__container-bloco-caixa2-caixa--texto{
+    width: 70%;
+    font-size: 14px;
+    margin-left: 2%;
+  }
+  .conteudo-duvidas__container-bloco2{
+    width: 100%;
     height: 9vh;
     border: solid 2px #2E3192;
     border-radius: 8px;
-    padding: 0 3px;
+    padding: 0 5px;
     display: flex;
     justify-content: center;
     align-items: center;
   }
-  .input-caixa-comentario{
-    width: 77%;
+  .conteudo-duvidas__container-bloco2--input{
+    width: 100%;
     height: 8vh;
     border: none;
     font-size: 16px;
     color: #2E3192;
     padding-left: 2%;
   }
-  .pai-centro_duvidas-caixa_comentario-botao_iniciar{
+  .conteudo-duvidas__container-bloco2--button{
     width: 10vw;
     height: 7vh;
     background-color: #2E3192;
     border: none;
     border-radius: 8px;
     color: #fff;
-    align-self: center;
-
-  }
-  .pai-centro_duvidas-caixa_comentario-entrada-descricao{
-    display: flex;
     align-self: center;
   }
 </style>
